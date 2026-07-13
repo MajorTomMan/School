911670cfb2c5ad63e0ce97adeab58e389254cd25
MajorTomMan <47267913+MajorTomMan@users.noreply.github.com>
2026-07-13 12:52:41 +0800

    构建：移除不再需要的源码过滤配置

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,15 +36,6 @@ android {
         buildConfig = true
     }
 
-    sourceSets {
-        getByName("main") {
-            java {
-                exclude("**/PersistentScreens.kt")
-                exclude("**/RoomReviewScreen.kt")
-            }
-        }
-    }
-
     packaging {
         resources.excludes += "/META-INF/{AL2.0,LGPL2.1}"
     }
