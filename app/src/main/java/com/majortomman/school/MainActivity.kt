bd73524492334d4ee817f920278a0024eaade913
MajorTomMan <47267913+MajorTomMan@users.noreply.github.com>
2026-07-13 14:17:47 +0800

    教材：向应用注入资源包仓库

--- app/src/main/java/com/majortomman/school/MainActivity.kt
+++ app/src/main/java/com/majortomman/school/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.majortomman.school.data.PreferencesRepository
+import com.majortomman.school.data.material.MaterialPackRepository
 import com.majortomman.school.ui.SchoolApp
 import com.majortomman.school.ui.theme.SchoolTheme
 
@@ -12,13 +13,19 @@ class MainActivity : ComponentActivity() {
     private val preferencesRepository by lazy {
         PreferencesRepository(applicationContext)
     }
+    private val materialPackRepository by lazy {
+        MaterialPackRepository(applicationContext)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             SchoolTheme {
-                SchoolApp(preferencesRepository)
+                SchoolApp(
+                    repository = preferencesRepository,
+                    materialRepository = materialPackRepository,
+                )
             }
         }
     }
